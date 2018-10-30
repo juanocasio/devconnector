17f51b4b6bbd018fb6e3fe36a3a3e0970ff701a6
Ocasio, Juan A <jaocasio@brandsafway.com>
2018-10-30 16:08:28 -0400

    Upated profile.js and added Profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+//Load models
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 router.get('/test', (req, res) => res.json({msg: "Profile Works!"}));
 
