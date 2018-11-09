9d88523718e29e5ec739953f2dd9ab5ef4fee943
Ocasio, Juan A <jaocasio@brandsafway.com>
2018-11-09 17:32:25 -0500

    updated profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -9,6 +9,9 @@ const User = require('../../models/User');
 
 router.get('/test', (req, res) => res.json({msg: "Profile Works!"}));
 
+//@route:   Get api/profile
+//@desc:    Gets current user's profile
+//@access:  Private
 router.get('/', passport.authenticate('jwt', {session: false}), (req, res) =>{
   const errors = {};
 
@@ -22,6 +25,103 @@ router.get('/', passport.authenticate('jwt', {session: false}), (req, res) =>{
     }
   })
   .catch(err => console.log(err));
-})
+});
+
+//@route:   POST api/profile
+//@desc:    Create or updates user's profile
+//@access:  Private
+router.post('/', passport.authenticate('jwt', {session: false}), (req, res) =>{
+  
+  //Get fields coming in
+  const profileFields = {};
+  profileFields.user = req.user.id;
+
+  if(req.body.handle){
+    profileFields.handle = req.body.handle;
+  };
+  
+  if(req.body.company){
+    profileFields.company = req.body.company;
+  };
+
+  if(req.body.website){
+    profileFields.website = req.body.website;
+  };
+
+  if(req.body.location){
+    profileFields.location = req.body.location;
+  };
+
+  if(req.body.bio){
+    profileFields.bio = req.body.bio;
+  };
+
+  if(req.body.status){
+    profileFields.status = req.body.status;
+  };
+
+  if(req.body.githubusername){
+    profileFields.githubusername = req.body.githubusername;
+  };
+
+  if(req.body.status){
+    profileFields.status = req.body.status;
+  };
+
+  if(req.body.skills !== 'undefined'){
+    profileFields.skills = req.body.skills.split(',');
+  };
+
+  if(req.body.status){
+    profileFields.status = req.body.status;
+  };
+
+  profileFields.social = {};
+
+  if(req.body.youtube){
+    profileFields.social.youtube = req.body.youtube;
+  };
+  
+  if(req.body.twitter){
+    profileFields.social.twitter = req.body.twitter;
+  };
+
+  if(req.body.facebook){
+    profileFields.social.facebook = req.body.facebook;
+  };
+
+  if(req.body.instagram){
+    profileFields.social.instagram = req.body.instagram;
+  };
+
+  if(req.body.linkedin){
+    profileFields.social.linkedin = req.body.linkedin;
+  };
+
+  Profile.findOne({ user: req.user.id })
+  .then(profile => {
+    if(profile){
+      //update profile
+      Profile.findByIdAndUpdate({ user: req.user.id }, { $set: profileFields }, { new: true })
+      .then(profile => res.json(profile));
+    }else{
+      //Create a new profile
+      Profile.findOne( { handle: profileFields.handle })
+      .then(profile => {
+        if(profile){
+          errors.handle = 'That handle already exists';
+          res.status(400).json(errors);
+        }
+
+        //Save profile
+        new Profile(profileFields).save().then(profile => res.json(profile));
+      })
+    }
+  })
+
+
+
+
+});
 
 module.exports = router;
---
+++ validation/profile.js
@@ -0,0 +1,23 @@
+const Validator = require('validator');
+const isEmpty = require ('./is-empty')
+
+module.exports = function validateProfileInput(data) {
+  let errors = {};
+
+  data.handle = !isEmpty(data.handle) ? data.handle : '';
+  data.status = !isEmpty(data.status) ? data.status : '';
+  data.skills = !isEmpty(data.skills) ? data.skills : '';
+
+  if(Validator.isEmpty(data.email)){
+    errors.email = 'Email field is required';
+  }
+
+  if (Validator.isEmpty(data.password)){
+    errors.password = 'Password field is required';
+  }
+  
+  return {
+    errors,
+    isValid:isEmpty(errors)
+  }
+}
